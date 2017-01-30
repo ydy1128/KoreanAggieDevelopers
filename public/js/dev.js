563f66ec4ca8afa8196ef435c8ed984f08b5aaa1
ydy1128 <ydy1128@gmail.com>
2017-01-30 00:27:59 -0600

    datafile change

--- public/js/dev.js
+++ public/js/dev.js
@@ -1,6 +1,6 @@
 var devApp = angular.module('devApp', []);
 devApp.controller('devCtrl', function($scope, $http){
-	$http.get('../data/developers.json')
+	$http.get('../data/developers.php')
 	.then(function (response) {
 		$scope.developers = response.data.developers
 	});
