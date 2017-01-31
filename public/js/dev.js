793e18b6dad50eaa42a5795020e53b533861a5cc
ydy1128 <ydy1128@gmail.com>
2017-01-31 14:39:37 -0600

    data move to josn

--- public/js/dev.js
+++ public/js/dev.js
@@ -1,54 +1,9 @@
 var devApp = angular.module('devApp', []);
-var data = {
-	"developers":
-	[
-		{
-			"Name" : "DaeYoung Yoon",
-			"Major" : "Computer Science Major",
-			"Role" : "Lead & Front-End Developer",
-			"Languages" : ["Python", "JavaScript", "C++"],
-			"Fields" : ["Computer Human Interaction", "Data Science", "Network Security"],
-			"Image" : "johnny.png",
-			"Link" : "http://ydy1128.pythonanywhere.com",
-			"Side" : "left"
-		},
-		{
-			"Name" : "Seung Heon Sheen",
-			"Major" : "Computer Science Major",
-			"Role" : "Back-End Developer",
-			"Languages" : ["C++", "Python", "JavaScript"],
-			"Fields" : ["Scientific Computing", "Language Processing", "Machine Learning"],
-			"Image" : "heon.png",
-			"Link" : "",
-			"Side" : "right"
-		},
-		{
-			"Name" : "JaeYoung Jun",
-			"Major" : "Computer Engineering Major",
-			"Role" : "Back-End Developer",
-			"Languages" : ["C++", "Java", "MatLab"],
-			"Fields" : ["Software Engineering", "Wireless Systems", "IT Operation"],
-			"Image" : "jae.png",
-			"Link" : "",
-			"Side" : "left"
-		},
-		{
-			"Name" : "Sangjoon Bae",
-			"Major" : "Computer Science Major",
-			"Role" : "Front-End Developer",
-			"Languages" : ["Java", "C++", "HTML/CSS"],
-			"Fields" : ["Augmented Reality", "Web Design", "Geo-Technology"],
-			"Image" : "bae.png",
-			"Link" : "",
-			"Side" : "right"
-		}
-	]
-}
 
 devApp.controller('devCtrl', function($scope, $http){
 	$scope.developers = data.developers;
-	// $http.get('../data/developers.php')
-	// .then(function (response) {
-	// 	$scope.developers = response.data.developers
-	// });
+	$http.get('../data/developers.json')
+	.then(function (response) {
+		$scope.developers = response.data.developers
+	});
 });
